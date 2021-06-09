c13bea29a1bab9404a49bb036f6a5ad8ea454c0b
bilalfareed83 <bilal_fareed@hotmail.com>
2021-06-09 16:37:53 +0500

    max and min width tutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { MinMaxClamp } from "./component/MinMaxWidth/minMaxClamp";
+
+function App() {
+  return (
+    <div>
+      <h2>Css Tutorial with Kevin Powell</h2>
+      <MinMaxClamp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MinMaxWidth/minMaxClamp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./minMaxClamp.css";
+export const MinMaxClamp = () => {
+  return (
+    <div className="containerMinMax">
+      <div className="content">
+        <h2 className="title">A catchey title here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          repudiandae fugiat esse voluptate dicta expedita porro a earum vitae
+          veritatis, vel quidem accusantium repellat itaque! Velit hic accusamus
+          similique harum?
+        </p>
+      </div>
+    </div>
+  );
+};
